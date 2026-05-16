cf4a4cdab564e5d0e23f0f4ba0cfe2daee3605fd
RafiaMuhammadHussain <rafiamuhammadhussain@gmail.com>
2026-05-16 22:40:55 +0500

    Done

---
+++ app.js
@@ -0,0 +1,12 @@
+function happyMood() {
+    document.getElementById("heading").innerHTML = "Keep Smiling🌈";
+    document.body.style.backgroundColor = "#eee9a1";
+}
+function sadMood() {
+    document.getElementById("heading").innerHTML = "Everything will be fine💙";
+    document.body.style.backgroundColor = "#648fec";
+}
+function angryMood() {
+    document.getElementById("heading").innerHTML = "Relax & Breathe🔥";
+    document.body.style.backgroundColor = "#b881b5";
+}
